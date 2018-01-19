e5d4441afbb93715b45a7f87db5bbc0aae5bf809
Carlos del Cerro Tenorio <carlosc7fuen@gmail.com>
2018-01-19 19:07:58 +0000

    HolaMundoC.cpp created online with Bitbucket

---
+++ HolaMundoC.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Bienvenidos a la clase de Estructuras. Aquí aprenderemos sobre:" << endl;
+    cout << "Vectores, Matrices, Pilas, Colas..." << endl;
+    return 0;
+}
